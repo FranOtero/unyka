d5fb6978d86f7d203b2b10901ec3e0be96b7c7c5
Fran Otero <fran.otero@idearia.es>
2024-12-01 21:22:24 +0100

    funciona

---
+++ Idearia.Pos.WooCommerceTest/FilesToHtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idearia.Pos.WooCommerceTest
+{
+    internal static class FilesToHtml
+    {
+        public static void ConvertFilesToHtml(string folder)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<link rel=\"stylesheet\" href=\"styles.css\" />");
+            foreach (var marcaDir in Directory.GetDirectories(folder))
+            {
+                string marca = Path.GetFileName(marcaDir);
+                sb.AppendLine($"<div class=\"marca-card\" id=\"{marca}\">");
+
+                sb.AppendLine($"<p class=\"marca\">{marca}</p>");
+                bool empty = true;
+                foreach (string familiaDir in Directory.GetDirectories(marcaDir))
+                {
+                    empty = false;
+                    sb.AppendLine($"<div class=\"marca-familia-card\">");
+                    string familia = Path.GetFileName(familiaDir);
+                    sb.AppendLine($"<p class=\"familia\">{familia}</p>");
+
+                    if (Directory.GetDirectories(familiaDir).Any())
+                    {
+                        foreach (string subfamiliaDir in Directory.GetDirectories(familiaDir))
+                        {
+                            string subfamilia = Path.GetFileName(subfamiliaDir);
+                            sb.AppendLine($"<p class=\"subfamilia\">{subfamilia}</p>");
+                            sb.AppendLine("<div class=\"subfamilia-card\">");
+                            foreach (string fileName in Directory.GetFiles(subfamiliaDir))
+                            {
+                                sb.AppendLine(GetLink(folder, fileName));
+                            }
+                            sb.AppendLine("</div> <!--subfamilia-card-->");
+                        }
+                    }
+                    else
+                    {
+                        sb.AppendLine("<div class=\"subfamilia-card\">");
+                        foreach (string fileName in Directory.GetFiles(familiaDir))
+                        {
+                            sb.AppendLine(GetLink(folder, fileName));
+                        }
+                        sb.AppendLine("</div> <!--subfamilia-card-->");
+                    }
+                    sb.AppendLine($"</div> <!--marca-familia-card-->");
+                }
+                if (empty)
+                {
+                    sb.AppendLine("<div class=\"subfamilia-card\">");
+                    foreach (string fileName in Directory.GetFiles(marcaDir))
+                    {
+                        sb.AppendLine(GetLink(folder, fileName));
+                    }
+                    sb.AppendLine("</div> <!--subfamilia-card-->");
+                }
+
+
+                sb.AppendLine($"</div> <!--marca-card-->");
+            }
+            // sb.AppendLine(" <script>parent.window.scrollTo(0,window.outerHeight/2+document.getElementById(parent.window.location.hash.substring(1)).offsetTop);</script>");
+
+            File.WriteAllText("C:/tmp/index.html", sb.ToString());
+        }
+        internal static string GetLink(string folder, string fileName)
+        {
+            string href = "/wp-includes/idearia/fichas/" + fileName.Substring(folder.Length).Replace("\\", "/").Replace(" ", "%20");
+            return $"<div class=\"link\" title=\"{Path.GetFileName(fileName)}\"><a target=\"_blank\" href=\"{href}\">{Path.GetFileNameWithoutExtension(fileName).Substring(0, 15)}</a></div>";
+        }
+    }
+
+
+}
---
+++ Idearia.Pos.WooCommerceTest/RawPrinterHelper.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class RawPrinterHelper
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public class DOCINFOA
+    {
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string? pDocName;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string? pOutputFile;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string? pDataType;
+    }
+
+    [DllImport("winspool.Drv", EntryPoint = "OpenPrinterA", SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool OpenPrinter(string szPrinter, out IntPtr hPrinter, IntPtr pd);
+
+    [DllImport("winspool.Drv", EntryPoint = "ClosePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool ClosePrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.Drv", EntryPoint = "StartDocPrinterA", SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool StartDocPrinter(IntPtr hPrinter, int level, [In][MarshalAs(UnmanagedType.LPStruct)] DOCINFOA di);
+
+    [DllImport("winspool.Drv", EntryPoint = "EndDocPrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool EndDocPrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.Drv", EntryPoint = "StartPagePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool StartPagePrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.Drv", EntryPoint = "EndPagePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool EndPagePrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.Drv", EntryPoint = "WritePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool WritePrinter(IntPtr hPrinter, IntPtr pBytes, int dwCount, out int dwWritten);
+
+    public static bool SendStringToPrinter(string szPrinterName, string szString)
+    {
+        IntPtr pBytes;
+        int dwCount = szString.Length;
+        pBytes = Marshal.StringToCoTaskMemAnsi(szString);
+        bool success = SendBytesToPrinter(szPrinterName, pBytes, dwCount);
+        Marshal.FreeCoTaskMem(pBytes);
+        return success;
+    }
+
+    public static bool SendBytesToPrinter(string szPrinterName, IntPtr pBytes, int dwCount)
+    {
+        IntPtr hPrinter;
+        DOCINFOA di = new DOCINFOA
+        {
+            pDocName = "RAW Document",
+            pDataType = "RAW"
+        };
+        bool success = false;
+        if (OpenPrinter(szPrinterName.Normalize(), out hPrinter, IntPtr.Zero))
+        {
+            if (StartDocPrinter(hPrinter, 1, di))
+            {
+                if (StartPagePrinter(hPrinter))
+                {
+                    success = WritePrinter(hPrinter, pBytes, dwCount, out _);
+                    EndPagePrinter(hPrinter);
+                }
+                EndDocPrinter(hPrinter);
+            }
+            ClosePrinter(hPrinter);
+        }
+        return success;
+    }
+}
