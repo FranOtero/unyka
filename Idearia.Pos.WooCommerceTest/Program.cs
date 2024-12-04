8ec577838346078c20d4f3c7016d2d332f63a214
Fran Otero <fran.otero@idearia.es>
2024-12-04 16:46:36 +0100

    cambiado nombre

--- Idearia.Pos.WooCommerceTest/Program.cs
+++ Idearia.Pos.WooCommerceTest/Program.cs
@@ -1,41 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Idearia.Pos.TicketPrinter;
-
+string printerName = "POS-80";
 
 //doble altura de fuente
 //RawPrinterHelper.SendStringToPrinter("POS-80", "\x1B\x21\x16");
 
 //fuente normal
-RawPrinterHelper.SendStringToPrinter("POS-80", "\x1B\x21\x00");
-//RawPrinterHelper.SendStringToPrinter("POS-80", "Fuente normal que tal estáis 00000ooo111 bien no\n");
+RawPrinterHelper.SendStringToPrinter(printerName, "\x1B\x21\x00");
+//RawPrinterHelper.SendStringToPrinter("printerName", "Fuente normal que tal estáis 00000ooo111 bien no\n");
 
-//RawPrinterHelper.SendStringToPrinter("POS-80", "\x1B\x21\x08");
-//RawPrinterHelper.SendStringToPrinter("POS-80", "Fuente enphasized que tal estáis 00000ooo111 bien no\n");
+//RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x21\x08");
+//RawPrinterHelper.SendStringToPrinter("printerName", "Fuente enphasized que tal estáis 00000ooo111 bien no\n");
 
-//RawPrinterHelper.SendStringToPrinter("POS-80", "\x1B\x21\x16");
-//RawPrinterHelper.SendStringToPrinter("POS-80", "Fuente doble height que tal estáis 00000ooo111 bien no\n");
+//RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x21\x16");
+//RawPrinterHelper.SendStringToPrinter("printerName", "Fuente doble height que tal estáis 00000ooo111 bien no\n");
 
-//RawPrinterHelper.SendStringToPrinter("POS-80", "\x1B\x21\x32");
-//RawPrinterHelper.SendStringToPrinter("POS-80", "Fuente doble width que tal estáis 00000ooo111 bien no\n");
+//RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x21\x32");
+//RawPrinterHelper.SendStringToPrinter("printerName", "Fuente doble width que tal estáis 00000ooo111 bien no\n");
 
-//RawPrinterHelper.SendStringToPrinter("POS-80", "\x1B\x21\x128");
-//RawPrinterHelper.SendStringToPrinter("POS-80", "Fuente underline que tal estáis 00000ooo111 bien no\n");
+//RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x21\x128");
+//RawPrinterHelper.SendStringToPrinter("printerName", "Fuente underline que tal estáis 00000ooo111 bien no\n");
 
-//RawPrinterHelper.SendStringToPrinter("POS-80", "\x1B\x61\x01");
-//RawPrinterHelper.SendStringToPrinter("POS-80", "Centrado\n");
-RawPrinterHelper.SendStringToPrinter("POS-80", "\x1B\x61\x00");
+//RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x61\x01");
+//RawPrinterHelper.SendStringToPrinter("printerName", "Centrado\n");
+RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x61\x00");
 
-//RawPrinterHelper.SendStringToPrinter("POS-80", "\x1D\x21\x70");
-//RawPrinterHelper.SendStringToPrinter("POS-80", "enorme\n");
+//RawPrinterHelper.SendStringToPrinter("printerName", "\x1D\x21\x70");
+//RawPrinterHelper.SendStringToPrinter("printerName", "enorme\n");
 
-//RawPrinterHelper.SendStringToPrinter("POS-80", "\x1D\x42\x01");
-//RawPrinterHelper.SendStringToPrinter("POS-80", "invertido\n");
-RawPrinterHelper.SendStringToPrinter("POS-80", "\x1D\x42\x00");
+//RawPrinterHelper.SendStringToPrinter("printerName", "\x1D\x42\x01");
+//RawPrinterHelper.SendStringToPrinter("printerName", "invertido\n");
+RawPrinterHelper.SendStringToPrinter("printerName", "\x1D\x42\x00");
 
 
-RawPrinterHelper.SendStringToPrinter("POS-80", "\x1B\x44\x28\x01\x00");
-RawPrinterHelper.SendStringToPrinter("POS-80", "Siiiiiiiiiii producto muy largo\x09 132.21€\n");
-RawPrinterHelper.SendStringToPrinter("POS-80", "Nooooooooooo corto \x09  32.21€\n");
+RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x44\x28\x01\x00");
+RawPrinterHelper.SendStringToPrinter("printerName", "Siiiiiiiiiii producto muy largo\x09 132.21€\n");
+RawPrinterHelper.SendStringToPrinter("printerName", "Nooooooooooo corto \x09  32.21€\n");
 
 
 
