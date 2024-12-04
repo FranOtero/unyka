dfae5f91b85b990c84d2057bb1953d21cb09755c
Silvia Otero <silviaotou@gmail.com>
2024-12-04 16:51:51 +0100

    ok

--- Idearia.Pos.WooCommerceTest/Program.cs
+++ Idearia.Pos.WooCommerceTest/Program.cs
@@ -1,48 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Idearia.Pos.TicketPrinter;
-string printerName = "POS-80";
+string printerName = "POS-80C";
 
 //doble altura de fuente
 //RawPrinterHelper.SendStringToPrinter("POS-80", "\x1B\x21\x16");
 
 //fuente normal
 RawPrinterHelper.SendStringToPrinter(printerName, "\x1B\x21\x00");
-//RawPrinterHelper.SendStringToPrinter("printerName", "Fuente normal que tal estáis 00000ooo111 bien no\n");
+//RawPrinterHelper.SendStringToPrinter(printerName, "Fuente normal que tal estáis 00000ooo111 bien no\n");
 
-//RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x21\x08");
-//RawPrinterHelper.SendStringToPrinter("printerName", "Fuente enphasized que tal estáis 00000ooo111 bien no\n");
+//RawPrinterHelper.SendStringToPrinter(printerName, "\x1B\x21\x08");
+//RawPrinterHelper.SendStringToPrinter(printerName, "Fuente enphasized que tal estáis 00000ooo111 bien no\n");
 
-//RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x21\x16");
-//RawPrinterHelper.SendStringToPrinter("printerName", "Fuente doble height que tal estáis 00000ooo111 bien no\n");
+//RawPrinterHelper.SendStringToPrinter(printerName, "\x1B\x21\x16");
+//RawPrinterHelper.SendStringToPrinter(printerName, "Fuente doble height que tal estáis 00000ooo111 bien no\n");
 
-//RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x21\x32");
-//RawPrinterHelper.SendStringToPrinter("printerName", "Fuente doble width que tal estáis 00000ooo111 bien no\n");
+//RawPrinterHelper.SendStringToPrinter(printerName, "\x1B\x21\x32");
+//RawPrinterHelper.SendStringToPrinter(printerName, "Fuente doble width que tal estáis 00000ooo111 bien no\n");
 
-//RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x21\x128");
-//RawPrinterHelper.SendStringToPrinter("printerName", "Fuente underline que tal estáis 00000ooo111 bien no\n");
+//RawPrinterHelper.SendStringToPrinter(printerName, "\x1B\x21\x128");
+//RawPrinterHelper.SendStringToPrinter(printerName, "Fuente underline que tal estáis 00000ooo111 bien no\n");
 
-//RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x61\x01");
-//RawPrinterHelper.SendStringToPrinter("printerName", "Centrado\n");
-RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x61\x00");
+RawPrinterHelper.SendStringToPrinter(printerName, "\x1B\x61\x01");
+RawPrinterHelper.SendStringToPrinter(printerName, "Centrado\n");
+RawPrinterHelper.SendStringToPrinter(printerName, "\x1B\x61\x00");
 
-//RawPrinterHelper.SendStringToPrinter("printerName", "\x1D\x21\x70");
-//RawPrinterHelper.SendStringToPrinter("printerName", "enorme\n");
+//RawPrinterHelper.SendStringToPrinter(printerName, "\x1D\x21\x70");
+//RawPrinterHelper.SendStringToPrinter(printerName, "enorme\n");
 
-//RawPrinterHelper.SendStringToPrinter("printerName", "\x1D\x42\x01");
-//RawPrinterHelper.SendStringToPrinter("printerName", "invertido\n");
-RawPrinterHelper.SendStringToPrinter("printerName", "\x1D\x42\x00");
-
-
-RawPrinterHelper.SendStringToPrinter("printerName", "\x1B\x44\x28\x01\x00");
-RawPrinterHelper.SendStringToPrinter("printerName", "Siiiiiiiiiii producto muy largo\x09 132.21€\n");
-RawPrinterHelper.SendStringToPrinter("printerName", "Nooooooooooo corto \x09  32.21€\n");
+//RawPrinterHelper.SendStringToPrinter(printerName, "\x1D\x42\x01");
+//RawPrinterHelper.SendStringToPrinter(printerName, "invertido\n");
+//RawPrinterHelper.SendStringToPrinter(printerName, "\x1D\x42\x00");
 
 
+RawPrinterHelper.SendStringToPrinter(printerName, "\x1B\x44\x28\x01\x00");
+RawPrinterHelper.SendStringToPrinter(printerName, "Siiiiiiiiiii producto muy largo\x09 132.21€\n");
+RawPrinterHelper.SendStringToPrinter(printerName, "Nooooooooooo corto \x09  32.21€\n");
 
-Printer printer = new Printer("POS-80");
+
+
+Printer printer = new Printer(printerName);
 
 printer.Print();
-RawPrinterHelper.SendStringToPrinter("POS-80", "\n\n");
+RawPrinterHelper.SendStringToPrinter(printerName, "\n\n");
 
 printer.CutPaper();
 
